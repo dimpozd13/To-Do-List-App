5962d44637c01eaf6fc82ed1d3d2b0d6c5c026d3
Dimpozd <dimpozd@yandex.ru>
2022-03-01 17:56:18 +0300

    Chapter 3 - Beginning

--- To Do List App/ContentView.swift
+++ To Do List App/ContentView.swift
@@ -7,10 +7,31 @@
 
 import SwiftUI
 
+struct ToDo {
+    let name: String
+    let category: String
+}
+
 struct ContentView: View {
+    
+    @State private var todos = [
+        ToDo(name: "Write SwiftUI book", category: "person.badge.clock"),
+        ToDo(name: "Read Bible", category: "person.circle"),
+        ToDo(name: "Bring kids out to play", category: "person.3"),
+        ToDo(name: "Fetch wife", category: "person.3"),
+        ToDo(name: "Call mum", category: "person.3")
+    ]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List{
+            ForEach(todos, id: \.name){ todo in
+                HStack{
+                    Image(systemName: todo.category).frame(width: 50, height: 50)
+                    Text(todo.name)
+                }
+            }
+        }
     }
 }
 
