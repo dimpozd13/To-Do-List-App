d17c3a3117e5e2b6891528afc3b1904e27427262
Dimpozd <dimpozd@yandex.ru>
2022-03-01 17:17:08 +0300

    Initial Commit

---
+++ To Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To Do List App
+//
+//  Created by Дмитрий Поздняков on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
